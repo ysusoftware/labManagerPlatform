cc0a36e267cc45ddb6f08ab748b8d9a86b17e309
Erisu <1269732086@qq.com>
2018-12-06 19:03:12 +0800

    Initial commit

---
+++ src/test/java/test.java
@@ -0,0 +1,11 @@
+/**
+ * @author Erisu
+ * @date 2018/12/6 18:58
+ * @Description TODO
+ * @Version 1.0
+ **/
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
