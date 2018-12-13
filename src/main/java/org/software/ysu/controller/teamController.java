4d1bbcbcffc0e972fe6ef111739bc2e0e524c2c3
MyGit49 <2285246577@qq.com>
2018-12-13 15:42:50 +0800

    Team的增删改查service实现
    Signed-off-by: MyGit49 <2285246577@qq.com>

--- src/main/java/org/software/ysu/controller/teamController.java
+++ src/main/java/org/software/ysu/controller/teamController.java
@@ -2,6 +2,7 @@ package org.software.ysu.controller;
 
 import org.software.ysu.pojo.TeamExample;
 import org.software.ysu.pojo.TeamWithBLOBs;
+import org.software.ysu.pojo.layuiResponse;
 import org.software.ysu.service.Interface.ITeamService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,17 +16,25 @@ public class teamController {
     ITeamService teamService;
 
     @RequestMapping("getTeam.do")
-    public List<TeamWithBLOBs> getTeam(){
+    public layuiResponse getTeam(){
         TeamExample teamExample = new TeamExample();
         teamExample.createCriteria().andTeamIdEqualTo(2);
         List<TeamWithBLOBs> teams = teamService.getTeam(teamExample);
-        return teams;
+        layuiResponse layuiResponse = new layuiResponse("0","",teams);
+        return layuiResponse;
     }
 
     @RequestMapping("addTeam.do")
     public String addTeam(TeamWithBLOBs team){
+//        team.setTeamAim("ok");
+//        team.setTeamPhone("ok");
+//        team.setTeamLocation("ok");
+//        team.setTeamMemberdes("ok");
+//        team.setTeamDes("ok");
+//        team.setTeamName("ok");
+//        team.setTeamEmail("ok");
         teamService.addTeam(team);
-        return "redirect:getTeam.do";
+        return "success";
     }
 
     @RequestMapping("delTeam.do")
@@ -33,7 +42,7 @@ public class teamController {
         TeamExample teamExample = new TeamExample();
         teamExample.createCriteria().andTeamIdEqualTo(teamId);
         teamService.delTeam(teamExample);
-        return "redirect:getTeam.do";
+        return "success";
     }
 
     @RequestMapping("updateTeam.do")
